16251ed3c241dfb57f170da40fef99de0078edcb
pietrof42 <pietro.lazzarotto@factory42.uk>
2021-11-09 22:34:38 +0000

    Created anchors

---
+++ Assets/Scripts/ScriptableObjects/Anchors/TransformAnchor.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Jammers
+{
+    [CreateAssetMenu(menuName = "Runtime Anchors/Transform")]
+    public class TransformAnchor : RuntimeAnchorBase<Transform>
+    {
+    }
+}
