dbdcb8893ff611ba76963a815c43f271d08cf9fc
pietrof42 <pietro.lazzarotto@factory42.uk>
2021-11-13 11:41:07 +0000

    Toggling menu

---
+++ Assets/Scripts/UI/ToggleMenu.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Jammers
+{
+
+    public class ToggleMenu : MonoBehaviour
+    {
+        public InputReader _inputReader;
+        public GameObject _menuToToggle;
+        bool _paused;
+
+        private void OnEnable()
+        {
+            _inputReader.MenuPauseEvent += Pause;
+            _inputReader.MenuUnpauseEvent += UnPause;
+            _paused = _menuToToggle.activeInHierarchy;
+            UpdateState();
+        }
+
+        private void UpdateState()
+        {
+            Time.timeScale = _paused ? 0 : 1;
+            _menuToToggle.SetActive(_paused);
+            if (_paused)
+            {
+                _inputReader.EnableMenuInput();
+            }
+            else
+            {
+            _inputReader.EnableGameplayInput();
+            }
+        }
+
+        private void OnDisable()
+        {
+            _inputReader.MenuPauseEvent -= Pause;
+            _inputReader.MenuUnpauseEvent -= UnPause;
+        }
+
+        private void Pause()
+        {
+            _paused = true;
+        }
+
+        private void UnPause()
+        {
+            _paused = false;
+        }
+    }
+}
